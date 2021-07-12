c1963e8c2dcff19ee658e3e40b8df1fcadf41a43
Paul Roberts <proberts@nuskin.com>
2021-07-12 14:08:23 -0600

    prettier style changes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.static('dist'));
+app.use(express.static("public"));
+app.use(express.static("dist"));
 
 app.listen(3000);
-console.log('app is running at http://localhost:3000');
+console.log("app is running at http://localhost:3000");
