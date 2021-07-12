1abe5522dba05e83cd81e43a0c81886031fb53c6
Paul Roberts <proberts@nuskin.com>
2021-07-12 14:07:04 -0600

    added files for running basic script

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.static('dist'));
+
+app.listen(3000);
+console.log('app is running at http://localhost:3000');
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+console.log("you've reached the index file");
